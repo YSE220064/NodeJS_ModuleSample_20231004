628e1f4fc4ecaa449ff28ad1bba7e924e89cb1a3
220064@YSE <220064md@yse-c.net>
2023-10-04 11:47:03 +0900

    server

--- express_sample/server.js
+++ express_sample/server.js
@@ -1,4 +1,25 @@
+//ReadModule
 const express = require("express")
 const dotenv = require("dotenv")
 
-dotenv
+//Readdotenv
+dotenv.config()
+const HOST_NAME = process.env.HOST_NAME
+const PORT_NUMBER = process.env.PORT_NUMBER
+
+console.log(HOST_NAME)
+console.log(PORT_NUMBER)
+
+//CreateServer
+const app = express()
+
+//GetRequest
+app.get("/", (req, res) => {
+    //Request
+    console.log(req.body)
+    console.log(req.url)
+    console.log(req.query)
+
+    //Response
+    res.send("KONNICHIWA!")
+})
